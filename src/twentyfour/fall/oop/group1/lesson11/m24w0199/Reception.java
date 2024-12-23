a1cd1138c4a7f1a20288485d14b6626649900e33
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2024-12-24 07:14:25 +0900

    final project MySql Database connect

---
+++ src/twentyfour/fall/oop/group1/lesson11/m24w0199/Reception.java
@@ -0,0 +1,13 @@
+package twentyfour.fall.oop.group1.lesson11.m24w0199;
+
+import javax.swing.*;
+
+public class test extends JFrame {
+    test(){
+        setSize(500,500);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new test();
+    }
+}
